d08e9068df943b559e68a5512b2b20b2acc6ede0
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2024-10-14 12:06:48 -0400

    Update python_javascript.js

--- PY EN JS/python_javascript.js
+++ PY EN JS/python_javascript.js
@@ -1,16 +1,19 @@
 // Cargar Pyodide desde un CDN
 async function runPythonCode() {
-    // Cargar Pyodide
-    let pyodide = await loadPyodide();
-  
-    // Ejecutar código Python en el navegador
-    let result = await pyodide.runPythonAsync(`
-      def my_algorithm(x):
+  // Cargar Pyodide
+  let pyodide = await loadPyodide();
+
+  // Ejecutar código Python en el navegador
+  // ``  Alt + 96
+  var codigoPy = `
+    def my_algorithm(x):
           return x * 2
-      my_algorithm(7)
-    `);
-  
-    // Mostrar el resultado en el div con id 'output'
-    document.getElementById('output').innerText = 'Resultado: ' + result;
-  }
-  
+          
+    my_algorithm(7)
+  `;
+
+  let result = await pyodide.runPythonAsync(codigoPy);
+
+  // Mostrar el resultado en el div con id 'output'
+  document.getElementById('output').innerText = 'Resultado: ' + result;
+}
